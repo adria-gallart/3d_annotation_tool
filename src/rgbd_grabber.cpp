67604ce5c4add812db35365e4c1088be5d4359ef
rares <rares@rares-Precision-M4700.(none)>
2013-08-21 10:07:48 +0200

    removed a comment

--- src/rgbd_grabber.cpp
+++ src/rgbd_grabber.cpp
@@ -321,8 +321,6 @@ std::string RGBDGrabber::getDateTime()
     struct tm  tstruct;
     char       buf[80];
     tstruct = *localtime(&now);
-    // Visit http://www.cplusplus.com/reference/clibrary/ctime/strftime/
-    // for more information about date/time format
     strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
 
     return buf;
